39cef65eb2cd1cf0b4a8853e59f3699fbd508f7c
ArtemKetty <artmedvemedvedev@yandex.ru>
2025-04-12 19:43:30 +0300

    new updates

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/go-sql-driver/mysql v1.9.0
 	github.com/gorilla/mux v1.8.1
 	github.com/gorilla/sessions v1.4.0
+	github.com/ilyakaznacheev/cleanenv v1.5.0
 	golang.org/x/crypto v0.35.0
 	gorm.io/driver/mysql v1.5.7
 	gorm.io/gorm v1.25.12
@@ -15,8 +16,12 @@ require (
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/BurntSushi/toml v1.5.0 // indirect
 	github.com/gorilla/securecookie v1.1.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/text v0.22.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
--- model/model.go
+++ model/model.go
@@ -3,25 +3,25 @@ package model
 import "time"
 
 type Category struct {
-	Id   uint   `gorm:"primary_key"`
-	Name string `gorm:"column:name"`
+	Id   uint   `gorm:"primary_key" json:"id"`
+	Name string `gorm:"column:name" json:"name"`
 }
 
 type Product struct {
-	Id        uint
-	Name      string
-	Price     uint
-	PhotoPath string
-	Count     int
+	Id        uint   `json:"id"`
+	Name      string `json:"name"`
+	Price     uint   `json:"price"`
+	PhotoPath string `json:"photo_path"`
+	Count     int    `json:"count"`
 }
 
 type TovarDetail struct {
-	Id         uint    `gorm:"primaryKey"`
-	Name       string  `gorm:"column:name"`
-	Price      uint    `gorm:"column:price"`
-	Counts     uint8   `gorm:"column:counts"`
-	IdCategory uint    `gorm:"column:idCategory"`
-	Photos     []Photo `gorm:"foreignKey:TovarId"` // Убедитесь, что внешний ключ для связи с Photos называется TovarId
+	Id         uint    `gorm:"primaryKey" json:"id"`
+	Name       string  `gorm:"column:name" json:"name"`
+	Price      uint    `gorm:"column:price" json:"price"`
+	Counts     uint8   `gorm:"column:counts" json:"counts"`
+	IdCategory uint    `gorm:"column:idCategory" json:"id_category"`
+	Photos     []Photo `gorm:"foreignKey:TovarId" json:"photos"`
 }
 
 func (TovarDetail) TableName() string {
@@ -29,9 +29,9 @@ func (TovarDetail) TableName() string {
 }
 
 type Photo struct {
-	Id       uint   `gorm:"primaryKey"`
-	Filepath string `gorm:"column:filepath"`
-	TovarId  uint   `gorm:"column:idTovar"` // Убедитесь, что в БД внешний ключ называется idTovar (не TovarId)
+	Id       uint   `gorm:"primaryKey" json:"id"`
+	Filepath string `gorm:"column:filepath" json:"filepath"`
+	TovarId  uint   `gorm:"column:idTovar" json:"tovar_id"`
 }
 
 func (Photo) TableName() string {
@@ -39,27 +39,27 @@ func (Photo) TableName() string {
 }
 
 type Basket struct {
-	Id      uint    `gorm:"primary_key"`
-	UserId  uint    `gorm:"foreignKey:UserId"`  // внешний ключ для user
-	TovarId uint    `gorm:"foreignKey:TovarId"` // внешний ключ для
-	Counts  int     `gorm:"column:counts"`
-	Tovar   Product `gorm:"foreignKey:TovarId"` // связь с товаром
+	Id      uint    `gorm:"primary_key" json:"id"`
+	UserId  uint    `gorm:"foreignKey:UserId" json:"user_id"`   // внешний ключ для user
+	TovarId uint    `gorm:"foreignKey:TovarId" json:"tovar_id"` // внешний ключ для товара
+	Counts  int     `gorm:"column:counts" json:"counts"`
+	Tovar   Product `gorm:"foreignKey:TovarId" json:"tovar"`
 }
 
 type UserModel struct {
-	Id        uint   `gorm:"primary_key"`
-	Login     string `gorm:"column:login"`
-	Password  string `gorm:"column:password"`
-	FirstName string `gorm:"column:first_name"`
-	NastName  string `gorm:"column:last_name"`
-	Email     string `gorm:"column:email"`
+	Id        uint   `gorm:"primary_key" json:"id"`
+	Login     string `gorm:"column:login" json:"login"`
+	Password  string `gorm:"column:password" json:"password"`
+	FirstName string `gorm:"column:first_name" json:"first_name"`
+	NastName  string `gorm:"column:last_name" json:"last_name"`
+	Email     string `gorm:"column:email" json:"email"`
 }
 
 type Session struct {
-	Id       uint      `gorm:"primaryKey"`
-	UserId   uint      `gorm:"foreignKey"`       // Внешний ключ на user
-	Token    string    `gorm:"column:token"`     // Токен с ограничением длины
-	DateTime time.Time `gorm:"column:date_time"` // Время создания сессии
+	Id       uint      `gorm:"primaryKey" json:"id"`
+	UserId   uint      `gorm:"foreignKey" json:"user_id"`         // Внешний ключ на user
+	Token    string    `gorm:"column:token" json:"token"`         // Токен с ограничением длины
+	DateTime time.Time `gorm:"column:date_time" json:"date_time"` // Время создания сессии
 }
 
 type PageData struct {
